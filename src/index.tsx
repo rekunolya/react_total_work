3b078cfc09f17997975d6c1f6fbeedb7f7d8f5f0
Roman <rekun.olya@gmail.com>
2021-11-28 20:00:51 +0300

    install faker, yup

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from 'react-router-dom';
 import { Provider } from "react-redux";
 import { store } from './store' 
-
+import  from './server'
+ 
 ReactDOM.render(
   <Router>
     <Provider store = {store}>
